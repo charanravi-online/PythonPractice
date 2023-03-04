07c7c056758d4b1cb32381ada0e357e8b81dc242
R. Charan <84191978+charanravi-online@users.noreply.github.com>
2023-03-04 20:53:54 +0530

    First commit
    The first commit contains nothing more than the first code from the practice session

---
+++ rad-to-degree.py
@@ -0,0 +1,4 @@
+print("This is a program to convert Radian to Degree")
+rad = input("Input Radian Value: ")
+radToDegree = float(rad) * (180/3.14)
+print("The above entered value of Rad in Degree is: " + str(radToDegree))
